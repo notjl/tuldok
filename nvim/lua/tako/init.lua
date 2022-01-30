11a630bf728c1d925e2e3f0bf663c06876be25ec
tako <58279268+notjl@users.noreply.github.com>
2022-01-30 17:04:38 +0800

    Added init for lua and preference options
    With init.vim being the primary reference for the new options
    "LunarVim" options helped me finding options that would make
    the neovim good looking in its bare form.

---
+++ nvim/lua/tako/init.lua
@@ -0,0 +1,8 @@
+-- Set shell to for <ter> or external commands
+if vim.fn.has('win32') or wim.fn.has('win64') then
+    vim.opt.sh = 'cmd.exe'
+else
+    vim.opt.sh = 'sh'
+end
+
+require "tako.options"
