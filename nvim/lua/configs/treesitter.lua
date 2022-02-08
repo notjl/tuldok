2d0b01737fb65fb897f02548b98adee96c7b9f2c
tako <58279268+notjl@users.noreply.github.com>
2022-02-08 09:40:30 +0800

    add treesitter config

---
+++ nvim/lua/configs/treesitter.lua
@@ -0,0 +1,21 @@
+local ok, configs = pcall(require, 'nvim-treesitter.configs')
+local install_ok, installer = pcall(require, 'nvim-treesitter.install')
+if not ok and not install_ok then
+    return
+end
+
+require 'nvim-treesitter.install'.compilers = {"clang"}
+configs.setup{
+    ensure_installed = 'maintained',
+    sync_install = false,
+    ignore_install = {""},
+    highlight = {
+        enable = true,
+        disable = {""},
+        additional_vim_regex_highlighting = true,
+    },
+    indent = {
+        enable = false,
+        disable = {"yaml"},
+    },
+}
