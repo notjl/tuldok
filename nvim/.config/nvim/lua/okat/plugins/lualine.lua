fa192beca813d4cfbdbace279217e33a214a9049
notjl <agcabezas@protonmail.com>
2023-01-31 20:49:42 +0800

    added lualine

---
+++ nvim/.config/nvim/lua/okat/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("lualine").setup({
+            options = {
+                disabled_filetypes = {
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                },
+            },
+        })
+    end
+}
