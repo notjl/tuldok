b8d2f84082a4db89a52d3b9b6540185f47f1250c
notjl <agcabezas@protonmail.com>
2023-01-31 20:06:14 +0800

    added bufferline using barbar

---
+++ nvim/.config/nvim/lua/okat/plugins/barbar.lua
@@ -0,0 +1,32 @@
+return {
+    "romgrk/barbar.nvim",
+    event = "BufNew",
+    keys = {
+        {"<A-h>", "<CMD>BufferPrevious<CR>"},
+        {"<A-l>", "<CMD>BufferNext<CR>"},
+        {"<LEADER>q", "<CMD>BufferClose<CR>"},
+    },
+    config = function()
+        -- Taken from https://github.com/romgrk/barbar.nvim/issues/355 -- 
+        local function is_neotree_open()
+            for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+                if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win), "ft") == "neo-tree" then
+                    return require("bufferline.api").set_offset(40, "FileTree")
+                end
+            end
+            return require("bufferline.api").set_offset(0)
+        end
+
+        vim.api.nvim_create_autocmd(
+        {
+            "BufWinEnter", "BufWipeout"
+        },
+        {
+            pattern = "*",
+            callback = function()
+                is_neotree_open()
+            end
+        }
+        )
+    end
+}
