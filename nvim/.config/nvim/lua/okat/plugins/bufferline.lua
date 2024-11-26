06327f9e81ff34c766638250f7a1587627097700
notjl <agcabezas@protonmail.com>
2024-11-26 14:51:38 +0800

    feat: added bufferline

---
+++ nvim/.config/nvim/lua/okat/plugins/bufferline.lua
@@ -0,0 +1,8 @@
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  opts = true,
+}
