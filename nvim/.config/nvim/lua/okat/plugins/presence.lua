58b7871124c3cee356533e79cc8064ac8950a8ad
notjl <agcabezas@protonmail.com>
2024-11-28 20:53:07 +0800

    feat: presence to cord plugin

--- nvim/.config/nvim/lua/okat/plugins/presence.lua
+++ nvim/.config/nvim/lua/okat/plugins/presence.lua
@@ -1,18 +1,29 @@
 return {
-  'andweeb/presence.nvim',
-  event = { 'BufReadPost', 'BufNewFile' },
+  'vyfor/cord.nvim',
+  build = './build || .\\build',
+  event = 'VeryLazy',
   opts = {
-    -- General Options
-    neovim_image_text = 'neovim > vscode',
-    main_image = 'file',
-
-    -- Rich Presence text options
-    editing_text = 'Touching ✍  %s',
-    file_explorer_text = 'Browsing 👀 %s',
-    git_commit_text = 'Thonking 🤔 COMMIT MSG',
-    plugin_manager_text = 'Managing 🧑💻 %s',
-    reading_text = 'Reading 🕵 %s',
-    workspace_text = 'Tinkering %s',
-    line_number_text = 'Line %s out of %s',
+    editor = { tooltip = 'The Superior Text Editor' },
+    text = {
+      viewing = 'Reading 🕵 {}',
+      editing = 'Touching ✍ {}',
+      file_browser = 'Browsing 👀 in {}',
+      plugin_manager = 'Managing 🧑💻 in {}',
+      lsp_manager = 'Configuring LSP 📚 in {}',
+      vcs = 'Thonking 🤔 Changes in {}',
+      workspace = 'In {}',
+    }
+    -- -- General Options
+    -- neovim_image_text = 'neovim > vscode',
+    -- main_image = 'file',
+    --
+    -- -- Rich Presence text options
+    -- editing_text = 'Touching ✍  %s',
+    -- file_explorer_text = 'Browsing 👀 %s',
+    -- git_commit_text = 'Thonking 🤔 COMMIT MSG',
+    -- plugin_manager_text = 'Managing 🧑💻 %s',
+    -- reading_text = 'Reading 🕵 %s',
+    -- workspace_text = 'Tinkering %s',
+    -- line_number_text = 'Line %s out of %s',
   }
 }
