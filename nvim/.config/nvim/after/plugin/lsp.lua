8bf05f3ac121833167a9e4e4fa49b868d16e7555
notjl <agcabezas@protonmail.com>
2025-07-02 12:34:42 +0800

    feat(lsp): opted for LspUI instead of native

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,42 +1,45 @@
--- vim.api.nvim_create_autocmd('LspAttach', {
---   group = vim.api.nvim_create_augroup('LSP Config', {}),
---   callback = function(args)
---     local ok, blink = pcall(require, 'blink.cmp')
---     if ok then
---       vim.lsp.config('*', {
---         capabilities = vim.tbl_deep_extend(
---           "force",
---           {},
---           vim.lsp.protocol.make_client_capabilities(),
---           blink.get_lsp_capabilities(),
---           -- {
---           --   fileOperations = {
---           --       didRename = true,
---           --       willRename = true,
---           --   },
---           -- }
---         ),
---       })
---     end
---   end,
--- })
-local capabilities = vim.tbl_deep_extend(
-  'force',
-  {},
-  vim.lsp.protocol.make_client_capabilities()
-)
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('LSP Config', {}),
+  callback = function(args)
+    local capabilities = vim.tbl_deep_extend(
+      'force',
+      {},
+      vim.lsp.protocol.make_client_capabilities()
+    )
 
-local ok, blink = pcall(require, 'blink.cmp')
-if ok then
-  capabilities = vim.tbl_deep_extend(
-    'force',
-    capabilities,
-    blink.get_lsp_capabilities()
-  )
-end
+    local ok, blink = pcall(require, 'blink.cmp')
+    if ok then
+      capabilities = vim.tbl_deep_extend(
+        'force',
+        capabilities,
+        blink.get_lsp_capabilities()
+      )
+    end
 
-vim.lsp.config('*', {
-  capabilities = capabilities
+    vim.lsp.config('*', {
+      capabilities = capabilities
+    })
+
+    vim.keymap.set('n', '<LEADER>ll', function()
+      local curr_config = vim.diagnostic.config()
+      local new_virtual_line_state
+
+      if curr_config.virtual_lines then
+        new_virtual_line_state = false
+      else
+        new_virtual_line_state = { current_line = true }
+      end
+
+      vim.diagnostic.config({ virtual_lines = new_virtual_line_state})
+    end, { desc = 'Toggle [L]SP Current Virtual [l]ine' })
+
+    vim.keymap.set('n', '<LEADER>lL', function()
+      local new_config = not vim.diagnostic.config().virtual_lines
+      vim.diagnostic.config({ virtual_lines = new_config })
+    end, { desc = 'Toggle [L]SP Virtual [L]ines' })
+
+
+  end,
 })
 
 vim.diagnostic.config({
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,7 +7,10 @@ return {
       flavour = 'mocha',
       transparent_background = true,
       show_end_of_buffer = true,
-      integrations = { barbar = true },
+      integrations = {
+        barbar = true,
+        render_markdown = true
+    },
     }
   },
   {
